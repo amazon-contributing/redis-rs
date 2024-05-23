0b567c3b096db5fdd416c4c8e6f7b59b7f79b3c2
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-05-23 16:50:22 -0700

    Add routing rules for `FUNCTION LIST`.
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- redis/src/cluster_routing.rs
+++ redis/src/cluster_routing.rs
@@ -309,8 +309,8 @@ impl ResponsePolicy {
             b"ACL SETUSER" | b"ACL DELUSER" | b"ACL SAVE" | b"CLIENT SETNAME"
             | b"CLIENT SETINFO" | b"CONFIG SET" | b"CONFIG RESETSTAT" | b"CONFIG REWRITE"
             | b"FLUSHALL" | b"FLUSHDB" | b"FUNCTION DELETE" | b"FUNCTION FLUSH"
-            | b"FUNCTION LOAD" | b"FUNCTION RESTORE" | b"MEMORY PURGE" | b"MSET" | b"PING"
-            | b"SCRIPT FLUSH" | b"SCRIPT LOAD" | b"SLOWLOG RESET" => {
+            | b"FUNCTION LOAD" | b"FUNCTION LIST" | b"FUNCTION RESTORE" | b"MEMORY PURGE"
+            | b"MSET" | b"PING" | b"SCRIPT FLUSH" | b"SCRIPT LOAD" | b"SLOWLOG RESET" => {
                 Some(ResponsePolicy::AllSucceeded)
             }
 
@@ -367,6 +367,7 @@ fn base_routing(cmd: &[u8]) -> RouteBy {
         | b"FUNCTION FLUSH"
         | b"FUNCTION KILL"
         | b"FUNCTION LOAD"
+        | b"FUNCTION LIST"
         | b"FUNCTION RESTORE"
         | b"FUNCTION STATS"
         | b"INFO"
